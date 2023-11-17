55176f921e737b7edffefe8e42c4d1f653e425cf
eunchaan <weoncheol1@naver.com>
2023-11-17 20:33:29 +0900

    백준 10808

--- src/main/java/com/example/codingtest/Main.java
+++ src/main/java/com/example/codingtest/Main.java
@@ -6,31 +6,16 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-        static int[] N = new int[9];
-        static int sum = 0;
-        static int temp1 = 0, temp2 = 0;
-
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        for (int i = 0; i < N.length; i++) {
-            N[i] = Integer.parseInt(br.readLine());
-            sum += N[i];
+        int arr[] = new int[26];
+        String word = br.readLine();
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            arr[c - 97]++;
         }
-        Arrays.sort(N);
-
-        for (int i = 0; i < N.length; i++) {
-            for (int j = i + 1; j < N.length; j++) {
-                if (sum - N[i] - N[j] == 100) {
-                    temp1 = N[i];
-                    temp2 = N[j];
-                }
-            }
-        }
-        for (int i = 0; i < N.length; i++) {
-            if(N[i] == temp1 || N[i] == temp2) continue;
-            System.out.println(N[i]);
+        for (int i = 0; i < 26; i++) {
+            System.out.print(arr[i] + " ");
         }
     }
 }
