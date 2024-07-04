b29772c3fc6fec8b533c604c0bfc2d15a8e3a8c8
kec <92673998+eckim97@users.noreply.github.com>
2024-07-04 21:21:26 +0900

    [Bronze V] Title: 나머지, Time: 220 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println((a+b)%c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+
+    }
+}
+
+
+
+
+
