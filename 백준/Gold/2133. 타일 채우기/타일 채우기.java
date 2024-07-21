39e475d936c362eb6e1c9d4059d090b60fee21c7
kec <92673998+eckim97@users.noreply.github.com>
2024-07-22 01:30:45 +0900

    [Gold IV] Title: 타일 채우기, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.java
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -11,12 +11,10 @@ public class Main {
             }
 
             int[] dp = new int[N + 1];
-    
-            if (N >= 2) dp[2] = 3;
-            if (N >= 4) dp[4] = 11;
-            
 
-            for(int i = 6; i <= N; i += 2) {
+            if (N >= 2) dp[2] = 3;
+
+            for(int i = 4; i <= N; i += 2) {
                 dp[i] = dp[i-2] * 3;
                 for(int j = i-4; j >= 0; j-=2) {
                     dp[i] += dp[j] * 2;
