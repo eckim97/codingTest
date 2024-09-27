cabfbdb053c0b1fdb6300214eae1d9ebde403bb8
kec <92673998+eckim97@users.noreply.github.com>
2024-09-28 03:12:24 +0900

    [Bronze III] Title: 네 수, Time: 100 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String A = st.nextToken();
+        String B = st.nextToken();
+        String C = st.nextToken();
+        String D = st.nextToken();
+
+        A = A + B;
+        C = C + D;
+
+        long sum = Long.parseLong(A) + Long.parseLong(C);
+
+        System.out.println(sum);
+    }
+}
