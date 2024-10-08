2509465c002023aa6ae19a303afa6dbec2996fa9
kec <92673998+eckim97@users.noreply.github.com>
2024-10-08 16:54:52 +0900

    [Bronze I] Title: 최소공배수, Time: 144 ms, Memory: 15020 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int remainder = a % b;
+            a = b;
+            b = remainder;
+        }
+        return a;
+    }
+
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            System.out.println(lcm(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+    }
+}
