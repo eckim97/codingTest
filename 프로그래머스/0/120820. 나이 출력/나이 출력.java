362c79e0fdafe7d7743e462a84c7a37d4a8cf545
kec <92673998+eckim97@users.noreply.github.com>
2025-01-25 22:04:34 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int age) {
+        int answer = 0;
+        int fix = 2023;
+        
+        answer = fix - age;
+        
+        return answer;
+    }
+}
+
