448225a3984f4cd83d3f6122d43d704130b995cb
kec <92673998+eckim97@users.noreply.github.com>
2025-05-10 15:25:20 +0900

    [level 0] Title: 가위 바위 보, Time: 0.06 ms, Memory: 87.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,16 @@
+class Solution {
+    public static String solution(String rsp) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : rsp.toCharArray()) {
+            if (c == '2') {
+                result.append('0'); 
+            } else if (c == '0') {
+                result.append('5');
+            } else if (c == '5') {
+                result.append('2'); 
+            }
+        }
+        return result.toString();
+    }
+}
