997ff651dcdebdea56acbca79347f3d3fd88f753
kec <92673998+eckim97@users.noreply.github.com>
2025-09-11 23:19:10 +0900

    [level 0] Title: 0 떼기, Time: 0.05 ms, Memory: 87.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        int temp = Integer.parseInt(n_str);
+        
+        answer = String.valueOf(temp);
+        return answer;
+    }
+}
