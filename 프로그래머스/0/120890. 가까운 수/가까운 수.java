6c198742963c77959337672f80e1378345fecf3f
kec <92673998+eckim97@users.noreply.github.com>
2025-11-16 23:42:52 +0900

    [level 0] Title: 가까운 수, Time: 0.54 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        Arrays.sort(array);
+        
+        int answer = array[0];
+        int minDiff = Math.abs(array[0] - n);
+        
+        for (int num : array) {
+            int diff = Math.abs(num - n);
+            
+            if (diff < minDiff) {
+                answer = num;
+                minDiff = diff;
+            }
+        }
+        
+        return answer;
+    }
+}
