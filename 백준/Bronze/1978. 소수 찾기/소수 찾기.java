b5c817c4c3fb6c19a342ed74cd4a042960ab46e3
kec <92673998+eckim97@users.noreply.github.com>
2026-03-14 18:19:53 +0900

    [Bronze II] Title: 소수 찾기, Time: 176 ms, Memory: 17824 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int count = 0;
+        
+        for (int i = 0; i < N; i++) {
+            int num = sc.nextInt();
+            if (isPrime(num)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        
+        sc.close();
+    }    
+    
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
