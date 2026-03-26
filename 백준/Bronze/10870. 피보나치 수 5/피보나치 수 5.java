b49d9a20ec9c1dc630ee7acad93a24da4ea74e77
kec <92673998+eckim97@users.noreply.github.com>
2026-03-26 23:20:49 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 168 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        System.out.println(fib(n));
+    }
+    public static int fib(int n){
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
