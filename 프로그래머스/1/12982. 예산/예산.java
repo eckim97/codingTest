54cfd3e3fb60499ad6a327be8a614e54569d58e2
kec <92673998+eckim97@users.noreply.github.com>
2026-04-12 18:03:17 +0900

    [level 1] Title: 예산, Time: 1.06 ms, Memory: 91.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        
+        int count = 0;
+        for (int i = 0; i < d.length; i++) {
+            if (budget >= d[i]) {
+                budget -= d[i];
+                count++;
+            } else {
+                break;
+            }
+        }
+        
+        return count;
+    }
+}
